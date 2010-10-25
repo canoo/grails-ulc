5bf92d266b4937a2f141690b38d00f88d00c88d1
aalmiray <aalmiray@gmail.com>
2010-10-25 17:53:49 +0200

    Removed unnecessary files. Bumped version to 0.2

--- UlcGrailsPlugin.groovy
+++ UlcGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import grails.util.GrailsNameUtils
  */
 class UlcGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.4 > *"
     // the other plugins this plugin depends on
