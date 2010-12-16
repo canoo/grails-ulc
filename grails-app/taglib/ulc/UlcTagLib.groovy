4889b969dd35fe7785baa1aae24312a668213d46
Dierk Koenig <dierk.koenig@canoo.com>
2010-12-16 03:38:33 +0100

    added <ulc:appletTag> that uses the old deprecated <applet> tag for calling the ui-engine. It works better in some contexts (e.g. on Mac OS X, Safari). To be revised.

--- grails-app/taglib/ulc/UlcTagLib.groovy
+++ grails-app/taglib/ulc/UlcTagLib.groovy
@@ -35,7 +35,7 @@ class UlcTagLib {
         StringWriter writer = new StringWriter()
         MarkupBuilder builder = new MarkupBuilder(writer)
         builder.script(src: 'http://java.com/js/deployJava.js','')
-        // the '' forces a closing tag. Some browsers do not handle the <script ../> correct
+        // the '' forces a closing tag. Some browsers do not handle the <script ../> correctly
 
         builder.script("""
 var attributes = {
@@ -54,6 +54,46 @@ deployJava.runApplet(attributes, parameters, '${javaVersion}')
         """)
 
         out << writer.toString()
+    }
+
+    /**
+     * Writes appropriate HTML code to display an ULC application as an (deprecated) applet tag.<p>
+     * This is still useful when the "applet" tag above is not displayed correctly
+     * The following attributes are supported
+     * <ul>
+     *   <li><strong>name</strong>: specifies the name of the application ro run. <strong>REQUIRED</strong>.</li>
+     *   <li><strong>archive</strong>: comma-separated list of client jars as passed onto the applet tag <strong>REQUIRED</strong>.</li>
+     *   <li><strong>width</strong>: width in pixels. Default: 480</li>
+     *   <li><strong>height</strong>: height in pixels. Default: 320</li>
+     * </ul>
+     * No additional parameters are set.
+     */
+    def appletTag = { attrs, body ->
+        Map aliases = getApplicationAliases(servletContext)
+
+        if(!attrs.name || !aliases[attrs.name]) {
+            throw new IllegalArgumentException("'${attrs.name}' is not a recognized ULC application alias.")
+        }
+
+        String alias    = attrs.name
+        String archive  = attrs.archive
+        String width    = attrs.width  ?: '480'
+        String height   = attrs.height ?: '320'
+        String className = aliases[alias] - 'Application'
+        String contextURL = request.requestURL.toString() - request.requestURI + request.contextPath
+
+        MarkupBuilder builder = new MarkupBuilder(out)
+        builder.applet(
+                width:  width,
+                height: height,
+                codebase: contextURL,
+                code:"${className}AppletLauncher.class",
+                archive: archive) {
+            param(name:"url-string", value:"${contextURL}/${alias}-applet.ulc")
+            param(name:"keep-alive-interval", value:"900")
+            param(name:"log-level", value:"WARNING")
+            'Your browser does not support JDK 1.5 or higher for applets.'
+        }
     }
 
 
--- src/templates/domainviewer/grails-app/views/domainviewer/ulcdomainviewer.gsp
+++ src/templates/domainviewer/grails-app/views/domainviewer/ulcdomainviewer.gsp
@@ -1,8 +1,14 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
-  <head><title>RIASuite grails domain viewer</title></head>
-  <body>
-  <h2>Browser your domain objects powered by Canoo RIASuite</h2>
-  <ulc:applet name="ulcdomainviewer" width="640" height="480"/>
-  </body>
+<head><title>RIASuite grails domain viewer</title></head>
+<body style="background-color:#ededed;">
+<h2>Browse your domain objects powered by Canoo RIASuite</h2>
+
+
+<div align="center">
+<ulc:appletTag width="640" height="480" name="ulcdomainviewer"
+    archive="ulc-client-libs/jcommon-7.0-canoo.jar,ulc-client-libs/jfreechart-7.0-canoo.jar,ulc-client-libs/ulc-core-client-7.0.1.u1b-r19027.jar,ulc-client-libs/ulcdomainviewer/application-ulcdomainviewer-client.jar"/>
+</div>
+
+</body>
 </html>
